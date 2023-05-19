e2c0374180b5afe503a432afcce6cf0b95c6fff5
aliifam <aliif.arief.m@gmail.com>
2023-05-19 15:15:56 +0700

    pinising

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // Membuat variabel, ubah sesuai dengan nama host dan database pada hosting 
-$host = "localhost";
-$user = "psait_11";
-$pass = "psait_11";
-$db   = "db_psait_11";
+$host = "localhost:3306";
+$user = "psait11";
+$pass = "psait11";
+$db   = "psait11";
  
 //Menggunakan objek mysqli untuk membuat koneksi dan menyimpan nya dalam variabel $mysqli 
 $mysqli = new mysqli($host, $user, $pass, $db);
